b15649c3e527322255f041294ea6d4300696a898
Oleksandra Krupnova <s19200@pjwstk.edu.pl>
2020-03-05 16:56:14 +0100

    initial commit

---
+++ tutorial1_solution/tutorial1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace tutorial1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var websiteURL = args[0];
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(websiteURL);
+            if(response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(content);
+
+                foreach (var email in matches)
+                {
+                    Console.WriteLine(email);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
